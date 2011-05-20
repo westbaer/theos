415e936be9dd44ef15939922cd8fd1ccdd692093
Dustin L. Howett <dustin@howett.net>
2011-05-20 11:03:31 -0700

    [tweak] Allow tweaks to masquerade as bundles if and only if xxx_BUNDLE_RESOURCE_??? is present, defaulting to /Library/Application Support/xxx/xxx.bundle (configurable with xxx_BUNDLE_INSTALL_PATH, xxx_BUNDLE_NAME and xxx_BUNDLE_EXTENSION.)

--- makefiles/instance/tweak.mk
+++ makefiles/instance/tweak.mk
@@ -15,5 +15,15 @@ include $(THEOS_MAKE_PATH)/instance/library.mk
 
 internal-tweak-all_:: internal-library-all_
 
-internal-tweak-stage_:: internal-library-stage_
+ifneq ($(strip $($(THEOS_CURRENT_INSTANCE)_BUNDLE_RESOURCE_DIRS) $($(THEOS_CURRENT_INSTANCE)_BUNDLE_RESOURCE_FILES)),)
+_LOCAL_BUNDLE_INSTALL_PATH = $(or $($(THEOS_CURRENT_INSTANCE)_BUNDLE_INSTALL_PATH),/Library/Application Support/$(THEOS_CURRENT_INSTANCE))
+_LOCAL_BUNDLE_NAME = $(or $($(THEOS_CURRENT_INSTANCE)_BUNDLE_NAME),$(THEOS_CURRENT_INSTANCE))
+_LOCAL_BUNDLE_EXTENSION = $(or $($(THEOS_CURRENT_INSTANCE)_BUNDLE_EXTENSION),bundle)
+
+_EXTRA_TARGET = shared-instance-bundle-stage
+THEOS_SHARED_BUNDLE_RESOURCE_PATH = $(THEOS_STAGING_DIR)$(_LOCAL_BUNDLE_INSTALL_PATH)/$(_LOCAL_BUNDLE_NAME).$(_LOCAL_BUNDLE_EXTENSION)
+include $(THEOS_MAKE_PATH)/instance/shared/bundle.mk
+endif
+
+internal-tweak-stage_:: $(_EXTRA_TARGET) internal-library-stage_
 	$(ECHO_NOTHING)if [ -f $(THEOS_CURRENT_INSTANCE).plist ]; then cp $(THEOS_CURRENT_INSTANCE).plist "$(THEOS_STAGING_DIR)$(LOCAL_INSTALL_PATH)/"; fi$(ECHO_END)
