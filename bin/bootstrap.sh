c4acf0b1bc1e6618f687e3ad7ff326b1a13d6bfa
Nicolas Haunold <westbaer@gmail.com>
2011-10-07 00:45:54 +0200

    [bootstrap] Added MSHookIvar to substrate.h generation code.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -115,6 +115,13 @@ MSImageRef MSGetImageByName(const char *file);
 #define MSInitialize \
     __attribute__((constructor)) static void _MSInitialize(void)
 
+template <typename Type_>
+static inline Type_ &MSHookIvar(id self, const char *name) {
+	Ivar ivar(class_getInstanceVariable(object_getClass(self), name));
+	void *pointer(ivar == NULL ? NULL : reinterpret_cast<char *>(self) + ivar_getOffset(ivar));
+	return *reinterpret_cast<Type_ *>(pointer);
+}
+
 #ifdef __APPLE__
 #ifdef __arm__
 IMP MSHookMessage(Class _class, SEL sel, IMP imp, const char *prefix = NULL);
