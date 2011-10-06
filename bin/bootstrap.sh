118446032acae757f2c4580914759e3ab0a4f787
Nicolas Haunold <westbaer@gmail.com>
2011-10-07 00:55:29 +0200

    [bootstrap] Fixed 'error: template with C linkage' error.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -115,12 +115,6 @@ MSImageRef MSGetImageByName(const char *file);
 #define MSInitialize \
     __attribute__((constructor)) static void _MSInitialize(void)
 
-template <typename Type_>
-static inline Type_ &MSHookIvar(id self, const char *name) {
-	Ivar ivar(class_getInstanceVariable(object_getClass(self), name));
-	void *pointer(ivar == NULL ? NULL : reinterpret_cast<char *>(self) + ivar_getOffset(ivar));
-	return *reinterpret_cast<Type_ *>(pointer);
-}
 
 #ifdef __APPLE__
 #ifdef __arm__
@@ -130,6 +124,14 @@ void MSHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result);
 #endif
 #ifdef __cplusplus
 }
+
+template <typename Type_>
+static inline Type_ &MSHookIvar(id self, const char *name) {
+	Ivar ivar(class_getInstanceVariable(object_getClass(self), name));
+	void *pointer(ivar == NULL ? NULL : reinterpret_cast<char *>(self) + ivar_getOffset(ivar));
+	return *reinterpret_cast<Type_ *>(pointer);
+}
+
 #endif
 __EOF
 }
